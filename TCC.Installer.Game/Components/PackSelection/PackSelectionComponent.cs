e361266faad7d500209d466048e2fab02954ee3e
Coppertine <37494321+Coppertine@users.noreply.github.com>
2020-03-16 14:27:56 +1000

    Update to Alpha Values instead of opacity

--- TCC.Installer.Game/Components/PackSelection/PackSelectionComponent.cs
+++ TCC.Installer.Game/Components/PackSelection/PackSelectionComponent.cs
@@ -34,7 +34,7 @@ namespace TCC.Installer.Game.Components.PackSelection
                     new PackItemComponent
                     {
                         DisplayName = "Minimum",
-                        DisplayColour = Color4.Green,
+                        DisplayColour = new Color4(0, 1, 0, 0.7),
                         Anchor = Anchor.CentreLeft,
                         Origin = Anchor.CentreLeft,
                         RelativeSizeAxes = Axes.Both,
@@ -42,7 +42,7 @@ namespace TCC.Installer.Game.Components.PackSelection
                     },new PackItemComponent
                     {
                         DisplayName = "Standart",
-                        DisplayColour = Color4.Blue,
+                        DisplayColour = new Color4(0, 0, 1, 0.7),
                         Anchor = Anchor.Centre,
                         Origin = Anchor.Centre,
                         RelativeSizeAxes = Axes.Both,
@@ -50,7 +50,7 @@ namespace TCC.Installer.Game.Components.PackSelection
                     },new PackItemComponent
                     {
                         DisplayName = "Deluxe",
-                        DisplayColour = Color4.Red,
+                        DisplayColour = new Color4(0, 0, 1, 0.7),
                         Anchor = Anchor.CentreRight,
                         Origin = Anchor.CentreRight,
                         RelativeSizeAxes = Axes.Both,
